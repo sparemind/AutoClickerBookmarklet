59657c16cfb493f067545a83702b4dbf70543eb9
Jake Chiang <jakewchiang@gmail.com>
2017-11-04 23:43:55 -0700

    Initial commit.

---
+++ AutoClicker.js
@@ -0,0 +1,29 @@
+javascript:
+
+var autoClickerStyleElement = document.createElement("style");
+autoClickerStyleElement.innerHTML="*{cursor: crosshair !important;}";
+document.body.appendChild(autoClickerStyleElement);
+function addClicker(e) {
+	if(!e.isTrusted) {
+		return;
+	}
+	if(e.target.classList.contains("auto-clicker-target")) {
+		e.target.classList.remove("auto-clicker-target");
+	} else {
+		e.target.classList.add("auto-clicker-target");
+	}
+	document.body.removeChild(autoClickerStyleElement);
+	document.body.removeEventListener("click", addClicker);
+	e.preventDefault();
+	
+	autoClick(e.target);
+
+}
+function autoClick(element) {
+	if(element.classList.contains("auto-clicker-target")) {
+		element.click();
+		setTimeout(function(){ autoClick(element); }, 10);
+	}
+}
+document.body.addEventListener("click", addClicker, 0);
+
